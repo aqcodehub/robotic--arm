364e9e1ba01e279519a1906319e5b01e80664353
aqcodehub <aqeilzaid567@gmail.com>
2026-07-21 22:28:08 +0800

    initial commit

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 My first robotics project.
 
 Hello Git Hub
+
+
---
+++ variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name = "Aq";
+    int age = 23;
+    float height = 171;
+    bool excitedComEng = true;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Excited for Computer Engineering: " << excitedComEng << endl;
+
+    return 0;
+}
+
+
