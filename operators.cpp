450b819f069bafca5937aa3f44f891e6669a8072
aqcodehub <aqeilzaid567@gmail.com>
2026-07-26 22:26:50 +0800

    inital commit

---
+++ operators.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+main()
+{
+    int num1 = 1;
+    int num2 = 4;
+    int x = 8;
+
+    x+= 2;
+    x++;
+
+    num1 += 10;
+
+    cout << "Num1:" << num1 << endl;
+    cout << "Num2:" << num2 << endl;
+    cout << "X:" << x << endl;
+
+    return 0;
+
+}
